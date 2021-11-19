fb217c56d96f65c45e010e6cb4107549eb0ca4e0
Edgar Alonzo <ragde956@gmail.com>
2021-11-18 21:27:02 -0600

    Get Movie Done, index not working for me

--- src/pages/composables/useAPI.js
+++ src/pages/composables/useAPI.js
@@ -2,6 +2,7 @@ import { ref } from "vue";
 import axios from "axios";
 
 const movies = ref();
+const movie = ref();
 
 // here we need the access token code from the website we signed up for.
 // we also need the link for the api calls
@@ -12,12 +13,17 @@ const api = axios.create({
   },
 });
 
-export const useAPI = () => {
+export const useAPI = (id) => {
   const getMovies = async () => {
     const response = await api.get("movie");
     movies.value = response.data.docs;
   };
 
+  const getMovie = async () => {
+    const response = await api.get(`movie/${id}`);
+    movie.value = response.data.docs[0];
+  };
+
   getMovies();
-  return { movies, getMovies };
+  return { movies, movie, getMovies, getMovie };
 };
